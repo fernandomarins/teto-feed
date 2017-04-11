c0605a621292a6efb8efea24bf0f6d5cc0bab7d9
Fernando Marins <f.augustomarins@gmail.com>
2017-04-11 13:13:19 -0300

    Added elements to cell and created extension to add visual format constraints

--- TetoFeed/FeedController.swift
+++ TetoFeed/FeedController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 let cellId = "cellId"
 
-class FeedController: UICollectionViewController {
-
+class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView?.bounces = true
         
         navigationController?.title = "Facebook Feed"
         collectionView?.backgroundColor = UIColor.white
@@ -28,8 +30,11 @@ class FeedController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 60)
+    }
+    
 }
 
 class FeedCell: UICollectionViewCell {
@@ -42,8 +47,46 @@ class FeedCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Sample text"
+        return label
+    }()
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = UIColor.red
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     func setupViews() {
-        backgroundColor = UIColor.white
+        backgroundColor = UIColor.blue
+        
+        addSubview(nameLabel)
+        addSubview(profileImageView)
+        
+        addConstraintsWithFormat(format: "H:|-8-[v0(44)]-8-[v1]|", views: profileImageView, nameLabel)
+        addConstraintsWithFormat(format: "V:|[v0]|", views: nameLabel)
+        addConstraintsWithFormat(format: "V:|-8-[v0(44)]|", views: profileImageView)
+        
+    }
+}
+
+extension UIView {
+    
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
     }
 }
 
