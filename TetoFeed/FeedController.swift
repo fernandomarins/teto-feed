ec6ef4a66e6f1622d7827511b9afc3027618a53e
Fernando Marins <f.augustomarins@gmail.com>
2017-04-11 02:49:02 -0300

    Created Xcode project, removed the storyboard and created a VC and NC in the app delegate

---
+++ TetoFeed/FeedController.swift
@@ -0,0 +1,21 @@
+//
+//  FeedController.swift
+//  TetoFeed
+//
+//  Created by Fernando Augusto de Marins on 10/04/17.
+//  Copyright © 2017 Fernando Augusto de Marins. All rights reserved.
+//
+
+import UIKit
+
+class FeedController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.green
+    }
+
+
+}
+
